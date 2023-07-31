4fde0439c3e1c73935e11327b76d9f6212f6e38b
baekdh0206 <baekdh@iei.or.kr>
2023-07-31 18:25:12 +0900

    20230731_반복문 연습문제 1,2,5 풀이

--- 01_Java/a03_control/src/edu/kh/control/loop/practice/LoopPractice.java
+++ 01_Java/a03_control/src/edu/kh/control/loop/practice/LoopPractice.java
@@ -3,6 +3,44 @@ package edu.kh.control.loop.practice;
 import java.util.Scanner;
 
 public class LoopPractice {
+	
+	/* 실습문제 1
+	사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 숫자들을 모두 출력하세요.
+	단, 입력한 수는 1보다 크거나 같아야 합니다. 
+	만일 1 미만의 숫자가 입력됐다면 “1 이상의 숫자를 입력해주세요“를 출력하세요.
+	*/
+	public void practice1() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int input = sc.nextInt();
+
+		if (input >= 1) {
+			for (int i = 1; i <= input; i++) {
+				System.out.print(i + " ");
+			}
+		} else {
+			System.out.println("1 이상의 숫자를 입력해주세요.");
+		}
+	}
+	
+	
+	/* 실습문제 2
+	사용자로부터 한 개의 값을 입력 받아 1부터 그 숫자까지의 모든 숫자를 거꾸로 출력하세요.
+	단, 입력한 수는 1보다 크거나 같아야 합니다. 
+	*/
+	public void practice2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int input = sc.nextInt();
+
+		if (input >= 1) {
+			for (int i = input; i >= 1; i--) {
+				System.out.print(i + " ");
+			}
+		} else {
+			System.out.println("1 이상의 숫자를 입력해주세요.");
+		}
+	}
 
 	/*
 	1부터 사용자에게 입력 받은 수까지의 정수들의 합을 for문을 이용하여 출력하세요.
@@ -87,6 +125,19 @@ public class LoopPractice {
 		
 	}
 	
+	/* 실습문제 5
+	 사용자로부터 입력 받은 숫자의 단을 출력하세요.
+	 */
+	public void practice5() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자 : ");
+		int num = sc.nextInt();
+		
+		System.out.printf("===== %d단 =====\n", num);
+		for(int i = 1; i < 10; i++) {			
+			System.out.printf("%d * %d = %d%n", num, i, (num * i));
+		}
+	}
 	
 	/*
 	사용자로부터 입력 받은 숫자의 단부터 9단까지 출력하세요.
