e30c7140f301787a5e2d97bd380bb6140cf498dd
baekdh230724 <baekdh@iei.or.kr>
2023-09-01 16:48:05 +0900

    20230901_JDBC

---
+++ 03_JDBC/MEMBER,BOARD,COMMENT.sql
@@ -0,0 +1,38 @@
+-- 시퀀스 생성
+CREATE SEQUENCE SEQ_MEMBER_NO NOCACHE; -- 회원번호
+CREATE SEQUENCE SEQ_BOARD_NO NOCACHE;  -- 게시글번호
+CREATE SEQUENCE SEQ_COMMENT_NO NOCACHE; -- 댓글번호
+
+-- MEMBER 테이블에 INSERT하기
+INSERT INTO MEMBER
+VALUES(SEQ_MEMBER_NO.NEXTVAL, 
+	'mem01@google.com', 
+	'123123', 
+	'1번회원',
+	'01012341234', 
+	'서울시 중구', 
+	DEFAULT, DEFAULT)
+;
+
+COMMIT;
+
+SELECT * FROM MEMBER;
+
+-- 이메일, 비밀번호가 일치하는 회원의
+-- 닉네임, 전화번호, 주소 수정
+UPDATE MEMBER 
+SET MEMBER_NICKNAME = '변경된닉네임',
+MEMBER_TEL = '01011112222',
+MEMBER_ADDRESS = '경기도 수원시'
+WHERE MEMBER_EMAIL = 'mem02@naver.com'
+AND MEMBER_PW = '1q2w3e4r';
+
+ROLLBACK;
+
+
+
+
+
+
+
+
