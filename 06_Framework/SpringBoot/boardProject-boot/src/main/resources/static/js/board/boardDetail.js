721d3868f553f71ab0eac4f448cd85514a2e6ef8
baek-dh <baekdh@iei.or.kr>
2023-11-06 16:39:11 +0900

    20231106_게시글 작성, 수정

--- 06_Framework/SpringBoot/boardProject-boot/src/main/resources/static/js/board/boardDetail.js
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/resources/static/js/board/boardDetail.js
@@ -144,3 +144,18 @@ if(deleteBtn != null){ // 삭제 버튼이 존재하는 경우
   });
 }
 
+// ---------------------------------------------------------
+
+/* 수정 버튼 클릭 시 수정 화면 요청 */
+const updateBtn = document.getElementById("updateBtn");
+
+if(updateBtn != null){ // 수정 버튼 존재 시
+  updateBtn.addEventListener('click', ()=>{
+
+    //    /board/{boardCode}/{booardNo}?cp=1
+    //    /editBoard/{boardCode}/{booardNo}/update?cp=1
+
+    let url = `/editBoard/${boardCode}/${boardNo}/update${location.search}`;
+    location.href = url;
+  });
+}
