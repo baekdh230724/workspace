4fc177533996a0df1e20a749c15f16dde7e8c2b7
baekdh230724 <baekdh230724@gmail.com>
2023-12-07 11:10:54 +0900

    20231207_Props

--- 06_Framework/SpringBoot/boardProject-boot/src/test/java/edu/kh/project/BoardProjectBootApplicationTests.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/test/java/edu/kh/project/BoardProjectBootApplicationTests.java
@@ -3,10 +3,10 @@ package edu.kh.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BoardProjectBootApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
